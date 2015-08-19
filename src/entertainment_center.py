650c9cca2a52944bed5e860659b757664b626ef8
w.stoettinger <wolfgang.stoettinger@gmail.com>
2015-08-19 20:02:43 +0200

    code style corrections

--- src/entertainment_center.py
+++ src/entertainment_center.py
@@ -2,18 +2,46 @@ import media
 import fresh_tomatoes
 
 # creating the movie objects
-mSteveJobs = media.Movie("Steve Jobs", "Awesome Movie about Steve Jobs", "http://www.patentlyapple.com/.a/6a0120a5580826970c01b8d115b9de970c-800wi", "https://www.youtube.com/watch?v=aEr6K1bwIVs")
-mAntMan = media.Movie("Marvel Ant-Man", "latest marvel movie", "http://t3.gstatic.com/images?q=tbn:ANd9GcRvTs_PtoegY0eToOxODXT12cfV-AipOD6GftFO0ze7wbociMNB", "https://www.youtube.com/watch?v=pWdKf3MneyI")
+mSteveJobs = media.Movie(
+    "Steve Jobs",
+    "Awesome Movie about Steve Jobs",
+    "http://www.patentlyapple.com/.a/6a0120a5580826970c01b8d115b9de970c-800wi",  # noqa
+    "https://www.youtube.com/watch?v=aEr6K1bwIVs")
+mAntMan = media.Movie(
+    "Marvel Ant-Man",
+    "latest marvel movie",
+    "http://t3.gstatic.com/images?q=tbn:ANd9GcRvTs_PtoegY0eToOxODXT12cfV-AipOD6GftFO0ze7wbociMNB",  # noqa
+    "https://www.youtube.com/watch?v=pWdKf3MneyI")
 
 # adding further details to the steve jobs movie (beyond minimum specification)
 mSteveJobs.director = "Danny Boyle"
-mSteveJobs.producers.extend(["Danny Boyle", "Guymon Casady", "Christian Colson", "Mark Gordon", "Scott Rudin"])
-mSteveJobs.actors.extend(["Michael Fassbender", "Kate Winslet", "Seth Rogen", "Jeff Daniels"])
+mSteveJobs.producers.extend(
+    ["Danny Boyle",
+     "Guymon Casady",
+     "Christian Colson",
+     "Mark Gordon",
+     "Scott Rudin"])
+mSteveJobs.actors.extend(
+    ["Michael Fassbender",
+     "Kate Winslet",
+     "Seth Rogen",
+     "Jeff Daniels"])
 
 # and further details to ant man
 mAntMan.director = "Peyton Reed"
 mAntMan.producers.extend(["Kevin Feige"])
-mAntMan.actors.extend(["Paul Rudd", "Evangeline Lilly", "Corey Stoll", "Bobby Cannavale", "Michael Pena", "Tip \"T.I.\" Harris", "Anthony Mackie", "Wood Harris", "Judy Greer", "David Dastmalchian", "Michael Douglas"])
+mAntMan.actors.extend(
+    ["Paul Rudd",
+     "Evangeline Lilly",
+     "Corey Stoll",
+     "Bobby Cannavale",
+     "Michael Pena",
+     "Tip \"T.I.\" Harris",
+     "Anthony Mackie",
+     "Wood Harris",
+     "Judy Greer",
+     "David Dastmalchian",
+     "Michael Douglas"])
 
 # creating a movies list
 movies = [mSteveJobs, mAntMan]
